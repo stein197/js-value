1238dcc72e7100b3904d790d4efe388e0fef61bd
Nail Gafarov <nil20122013@gmail.com>
2021-10-14 13:55:44 +0500

    Added documentation

--- src/Container.ts
+++ src/Container.ts
@@ -1,19 +1,51 @@
 import {EventEmitter} from "@stein197/observer";
 import Value from "./Value";
 
+/**
+ * Observable class which wraps map of values and observes on them.
+ * Usage:
+ * ```ts
+ * const container = new Container({
+ *     created: new Date(),
+ *     id: 1,
+ *     params: {
+ *         arg1: 12,
+ *         // ...
+ *     }
+ * });
+ * container.addEventListener("params", params => console.log(params.arg1));
+ * container.set("params", {arg1: 20});
+ * > 20
+ * ```
+ * @typaParam T - Type of passed value.
+ */
 export default class Container<T extends {[K: string]: any}> implements EventEmitter<{[K in keyof T]: (value: T[K]) => void}> {
 
 	private readonly values: {[K in keyof T]: Value<T[K]>} = {} as {[K in keyof T]: Value<T[K]>};
 
+	/**
+	 * Create observable wrapper around map of values.
+	 * @param values Initial map values.
+	 */
 	public constructor(values: T) {
 		for (const [key, value] of Object.entries(values))
 			this.values[key as keyof T] = new Value(value);
 	}
 
+	/**
+	 * Return value by its key.
+	 * @param key Key by which value is returned.
+	 * @returns Value.
+	 */
 	public get<K extends keyof T>(key: K): T[K] {
 		return this.values[key].get();
 	}
 
+	/**
+	 * Set value by its key
+	 * @param key Key by which value is set.
+	 * @param value New value.
+	 */
 	public set<K extends keyof T>(key: K, value: T[K]): void {
 		this.values[key].set(value);
 	}
--- src/Value.ts
+++ src/Value.ts
@@ -1,16 +1,40 @@
-import {Observable, Observer} from "@stein197/observer";
 import lodash from "lodash";
+import {Observable, Observer} from "@stein197/observer";
 
+/**
+ * Observable class which wraps passed values and observes them to change.
+ * Usage:
+ * ```ts
+ * const value = new Value("string");
+ * value.addListener(string => console.log(string));
+ * value.set("new string");
+ * > "string"
+ * ```
+ * Complex values can also be observed.
+ * @typaParam T - Type of passed value.
+ */
 export default class Value<T> implements Observable<(value: T) => void> {
 
 	private readonly observer = new Observer<(value: T) => void>();
 
+	/**
+	 * Instantiates wrapper with passed value.
+	 * @param value Initial value.
+	 */
 	public constructor(private value: T) {}
 
+	/**
+	 * Return value.
+	 * @returns Value.
+	 */
 	public get(): T {
 		return this.value;
 	}
 
+	/**
+	 * Set the value and fires listeners on change.
+	 * @param value New value
+	 */
 	public set(value: T): void {
 		if (lodash.isEqual(this.value, value))
 			return;
