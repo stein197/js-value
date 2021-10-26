6fb88a006d3ec3905b394ab21044f908fa371cfd
Nail Gafarov <nil20122013@gmail.com>
2021-10-26 13:56:25 +0500

    Added noop function

--- test.ts
+++ test.ts
@@ -2,37 +2,59 @@ import mocha from "mocha";
 import assert from "assert";
 import {Value, Container} from ".";
 
+function getNoop(count: number): {noop(): void; tracker: assert.CallTracker} {
+	const tracker = new assert.CallTracker();
+	return {
+		noop: tracker.calls(() => {}, count),
+		tracker
+	};
+}
+
 mocha.describe("Value<T>", () => {
-	let tracker = new assert.CallTracker();
-	let noop: () => void;
 	let primitiveValue: Value<string>;
 	let arrayValue: Value<number[]>;
 	let objectValue: Value<{name: string; age: number}>;
 	let complexValue: Value<{name: string; age: number; array: number[]; object: {param1: string; param2: number}}>;
 	let arrayOfObjectsValue: Value<{name: string; age: number}[]>;
-	let objectOfArraysValue: Value<{array: {name: string; age: number}[]}>;
+	let objectOfArraysValue: Value<{array: string[]}>;
 
 	mocha.beforeEach(() => {
-		noop = tracker.calls(() => {}, 1);
 		primitiveValue = new Value("John");
 		arrayValue = new Value([1, 2, 3]);
 		objectValue = new Value({name: "John", age: 12});
+		complexValue = new Value({name: "John", age: 12, array: [1, 2, 3], object: {param1: "A", param2: 1}});
+		arrayOfObjectsValue = new Value([
+			{name: "John", age: 12},
+			{name: "Jack", age: 32}
+		]);
+		objectOfArraysValue = new Value({
+			array: [
+				"A", "B", "C"
+			]
+		});
 	});
 
-	mocha.it.skip("Not changing primitive value won't fire an event");
+	mocha.it("Not changing primitive value won't fire an event", () => {
+		const noop = getNoop(0);
+		primitiveValue.addListener(noop.noop);
+		primitiveValue.set("John");
+		noop.tracker.verify();
+	});
+
 	mocha.it("Changing primitive value fires an event", () => {
-		primitiveValue.addListener(noop);
+		const noop = getNoop(1);
+		primitiveValue.addListener(noop.noop);
 		primitiveValue.set("string");
-		tracker.verify();
+		noop.tracker.verify();
 	});
 
-	mocha.it.skip("Setting value to null changes the value fires an event");
+	mocha.it.skip("Value<T>.set(null) changes fires an event");
 	mocha.it.skip("Unsetting value from null changes the value fires an event");
 
 	mocha.it.skip("Changing an array fires an event");
 	mocha.it.skip("Not changing an array won't fire an event");
 
-	mocha.it.skip("Changing inner array fires an event");
+	mocha.it.skip("Changing inner array in object fires an event");
 	mocha.it.skip("Setting partial object value does not erase omitted fields");
 });
 
